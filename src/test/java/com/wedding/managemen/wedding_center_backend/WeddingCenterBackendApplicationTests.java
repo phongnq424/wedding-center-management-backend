d6c50277ba4e2c43c141ea568b70e345e8942e1b
phongnq424 <ngcthanh1567@gmail.com>
2026-03-24 13:34:05 +0700

    first commit

---
+++ src/test/java/com/wedding/managemen/wedding_center_backend/WeddingCenterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedding.managemen.wedding_center_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingCenterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
